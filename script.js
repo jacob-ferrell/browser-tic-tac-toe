fed174d31ef66b43a8ac41c4c0cdcd402ddbcef2
jacob-ferrell <michaeljacobferrell@gmail.com>
2022-06-17 22:43:56 -0400

    added ability for players to enter custom names

--- script.js
+++ script.js
@@ -1,81 +1,74 @@
-const GameBoard = (() => {
-    //create 3x3 array filled with empty strings
-    const gameBoard = [['', '', ''], ['', '', ''], ['', '', '']]
-    //create div elements for every space, assign unique ID in row-column format, and append to board-container
-    const boardToDOM = () => {
-        let container = document.querySelector('.board-container');
-        gameBoard.forEach((row, r) => row.forEach((space, c) => {
-            let newSpace = document.createElement('div');
-            newSpace.id = `r${r}c${c}`
-            container.appendChild(newSpace);
-        }))
-    }
-    const makeMove = (currentPlayer) => {
-        document.querySelectorAll('.board-container div').forEach(space => space.addEventListener('click', (event) => {
-            if (!Game.gameOver(gameBoard) && !space.textContent) {
-            nextPlayer = Game.nextPlayer(currentPlayer);
-            Prompt.promptPlayer(nextPlayer);
-            let [r , c] = getCoordinate(event);
-            letter = currentPlayer.letter;
-            gameBoard[r][c] = letter;
-            writeToBoard(space, letter);
-            currentPlayer = nextPlayer;  
-            Game.gameOver(gameBoard);
-            } 
 
-        }))
-
-    const getCoordinate = (event) => event.target.id.match(/\d/g);
-    const writeToBoard = (space, letter) => {
-        space.textContent = letter;
-        colorClass = letter == 'X' ? 'player1' : 'player2'
-        space.classList.add(colorClass);
-    }
-    }
-
-    return {boardToDOM, makeMove};
-})();
 //create a player and assign player letter as x or o depending on player number
 const Player = (playerNumber, name) => {
     const letter = playerNumber == 1 ? 'X' : 'O';
     return {name, letter};
 }
 
-const Game = (() => {
-    const player1 = Player(1, 'Jacob');
-    const player2 = Player(2, 'Cindy');
+const Game = () => {
+    const player1 = Player(1, document.getElementById('player1-name').value);
+    const player2 = Player(2, document.getElementById('player2-name').value);
     
+    const GameBoard = (() => {
+        //create 3x3 array filled with empty strings
+        const gameBoard = [['', '', ''], ['', '', ''], ['', '', '']]
+        //create div elements for every space, assign unique ID in row-column format, and append to board-container
+        const boardToDOM = () => {
+            let container = document.querySelector('.board-container');
+            gameBoard.forEach((row, r) => row.forEach((space, c) => {
+                let newSpace = document.createElement('div');
+                newSpace.id = `r${r}c${c}`
+                container.appendChild(newSpace);
+            }))
+        }
+        const makeMove = (currentPlayer) => {
+            document.querySelectorAll('.board-container div').forEach(space => space.addEventListener('click', (event) => {
+                if (!gameOver(gameBoard) && !space.textContent) {
+                let nextPlayer = getNextPlayer(currentPlayer);
+                Prompt.promptPlayer(nextPlayer);
+                let [r , c] = getCoordinate(event);
+                letter = currentPlayer.letter;
+                gameBoard[r][c] = letter;
+                writeToBoard(space, letter);
+                currentPlayer = nextPlayer;  
+                gameOver(gameBoard);
+                } 
+    
+            }))
+    
+        const getCoordinate = (event) => event.target.id.match(/\d/g);
+        const writeToBoard = (space, letter) => {
+            space.textContent = letter;
+            colorClass = letter == 'X' ? 'player1' : 'player2'
+            space.classList.add(colorClass);
+        }
+        }
+    
+        return {boardToDOM, makeMove};
+    })();
 
-    const nextPlayer = (currentPlayer) => {
-        if (currentPlayer.letter === 'X') {
+    const startGame = (() => {
+        
+        GameBoard.boardToDOM();
+        Prompt.promptPlayer(player1);
+        GameBoard.makeMove(player1);
+    })();
+    
+    const getNextPlayer = (currentPlayer) => {
+        if (currentPlayer == player1) {
             return player2;
         } else {
             return player1;
         }
     }
-    const startGame = () => {
-        GameBoard.boardToDOM();
-        Prompt.promptPlayer(player1);
-        GameBoard.makeMove(player1);
-    }
-    const getNames = () => {
-        const submitNames = document.getElementById('submit-names');
-        submitNames.addEventListener('click', () => {
-            const names = [];
-            names.push(document.getElementById('player1-name').value);
-            names.push(document.getElementById('player2-name').value);
-            document.querySelector('.input-container').remove();
-            
-        })
- 
-    }
+    
+    
     const gameOver = (gameBoard) => {
         if (!getEmptySpaces(gameBoard).length) {
             Prompt.declareTie();
             return true;
         } else if (getWinner(gameBoard)) {
             winner = getWinner(gameBoard);
-            console.log(winner.name);
             Prompt.declareWinner(winner);
             return true;
         }
@@ -125,8 +118,8 @@ const Game = (() => {
         return columns;
     }
 
-    return {startGame, gameOver, nextPlayer};
-})();
+    return {startGame, gameOver, getNextPlayer};
+};
 
 const Prompt = (() => {
     const promptElement = document.getElementById('prompt');
@@ -136,4 +129,18 @@ const Prompt = (() => {
     const changePrompt = (text) => promptElement.textContent = text;
     return {promptPlayer, declareTie, declareWinner};
 })();
-Game.startGame();
+
+const getNames = (() => {
+    const submitNames = document.getElementById('submit-names');
+    submitNames.addEventListener('click', () => {
+        let player1Name = document.getElementById('player1-name').value;
+        let player2Name = document.getElementById('player2-name').value;
+        if (player1Name && player2Name) {
+            const newGame = Game();
+            document.querySelector('.input-container').remove();
+        }
+        })
+    
+})();
+
+
